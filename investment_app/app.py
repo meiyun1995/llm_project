9fd49e0e33baaf214192d145b58566137f324831
Chua Mei Yun <chuameiyun@gmail.com>
2023-07-17 11:19:43 +0800

    working version but model slow

--- investment_app/app.py
+++ investment_app/app.py
@@ -6,55 +6,36 @@ from langchain.embeddings import HuggingFaceEmbeddings
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
-
-from langchain.agents.agent_toolkits import (
-    create_vectorstore_agent,
-    VectorStoreToolkit,
-    VectorStoreInfo
-)
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
 
 
 PATH = '/Users/chuameiyun/gpt4all/ggml-model-gpt4all-falcon-q4_0.bin'
 
 llm = GPT4All(model=PATH, verbose=True)
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
 st.title('💵 Your Investment Guide')
 
 loader = PyPDFLoader('annualreport2223.pdf')
 pages = loader.load_and_split()
-store = Chroma.from_documents(pages, embeddings, collection_name='annualreport')
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64)
+texts = text_splitter.split_documents(pages)
 
-vectorstore_info = VectorStoreInfo(
-    name='annual_report',
-    description='a banking annual report as pdf',
-    vectorstore=store
-)
+db = Chroma.from_documents(texts, embeddings, collection_name='annualreport')
 
-toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
-
-agent_executor = create_vectorstore_agent(
+qa = RetrievalQA.from_chain_type(
     llm=llm,
-    toolkit=toolkit,
-    verbose=True
+    chain_type="stuff",
+    retriever=db.as_retriever(search_kwargs={"k": 3}),
+    return_source_documents=True,
+    verbose=False,
 )
-
-# prompt = PromptTemplate(input_variables=['question'],
-#                         template = """Question: {question}
-
-#                         A: Let's think step by step
-#                         """
-#                         )
-
-# chain = LLMChain(prompt=prompt, llm=llm)
 
 prompt = st.text_input('Input your prompt here')
 
 if prompt:
     # response = chain.run(prompt)
-    response = agent_executor.run(prompt)
+    response = qa(prompt)
 
-    st.write(response)
-
-    with st.expander('Document Similarity Search'):
-        search = store.similarity_search_with_score(prompt)
-        st.write(search[0][0].page_content)
+    st.write(response['result'])
