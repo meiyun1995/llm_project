789c369fcb2d7958b21beea4d6543a78d3cb48ff
Chua Mei Yun <chuameiyun@gmail.com>
2023-07-23 16:36:29 +0800

    Able to upload pdf and pass into llm

---
+++ coverletter_generation_app/app.py
@@ -0,0 +1,62 @@
+import os
+import tempfile
+import streamlit as st
+
+# Langchain imports
+from langchain.llms import GPT4All
+from langchain.embeddings import HuggingFaceEmbeddings
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+
+# Local dir to downloaded GPT4All models https://gpt4all.io/index.html
+PATH = '/Users/chuameiyun/gpt4all/ggml-model-gpt4all-falcon-q4_0.bin'
+
+# Init LLM model and embedding
+llm = GPT4All(model=PATH, verbose=True)
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+st.title('Generate Cover letter')
+
+# Process the annual report you wish to analyse
+st.header('Single File Upload')
+uploaded_file = st.file_uploader('Upload a file',
+                                 accept_multiple_files=False, 
+                                 type=['pdf']
+                                 )
+
+temp_file_path = os.getcwd()
+while uploaded_file is None:
+    x = 1
+        
+if uploaded_file:
+    # Save the uploaded file to a temporary location
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_file_path = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_file_path, "wb") as temp_file:
+        temp_file.write(uploaded_file.read())
+
+    loader = PyPDFLoader(temp_file_path)
+    pages = loader.load_and_split()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64)
+    texts = text_splitter.split_documents(pages)
+
+    # Vector store
+    db = Chroma.from_documents(texts, embeddings, collection_name='resume', persist_directory='db')
+
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=db.as_retriever(search_kwargs={"k": 3}),
+        return_source_documents=True,
+        verbose=False,
+    )
+
+    prompt = st.text_input('Input your prompt here')
+
+    if prompt:
+        response = qa(prompt)
+
+        st.write(response['result'])
